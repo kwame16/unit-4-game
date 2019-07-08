5d55565c07e19af9b44d256c8088b8b8a3d8d1b2
kwame16 <51427903+kwame16@users.noreply.github.com>
2019-07-08 17:05:09 -0400

    gave images values, put resets on if score wins or losses, displayed values, and assigned new numbers after wins and losses. Game funtionality is complete and running well. Just need to add style.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,36 +7,99 @@ var crystalChoices = [1,2,3,4,5,6,7,8,9,10,11,12];
 var wins = 0;
 var losses = 0;
 
+//users score
+var score = 0;
+
 // Create variables that hold references to the places in the HTML where we want to display things.
 
-var userWins= document.getElementById('wins-text');
-var userLosses = document.getElementById('losses-text');
-var userChoice = document.getElementById('userchoice-text');
+var userWins = $('#wins-text');
+var userLosses = $('#losses-text');
+var userScore = $('#usersScore-text');
+var userResult = $('#result-text');
+var computerChoice = $('#computerGuess-text');
 
-var computerGuess = crystalChoices[Math.floor(Math.random() * crystalChoices.length)];
+var computerGuess = Math.floor(Math.random() * 101) + 19;
 console.log(computerGuess);
 
 // Display the user and computer guesses, and wins/losses left.
 //   computerChoiceText.textContent = 'The computer chose: ' + computerGuess;
-userWins.textContent = "wins: " + wins;
-userLosses.textContent = "losses: " + losses;
+userWins.text("wins: " + wins);
+userLosses.text("losses: " + losses);
 
 // crystal values
-var crystal1 = crystalChoices;
-var crystal2 = crystalChoices;
-var crystal3 = crystalChoices;
-var crystal4 = crystalChoices;
+var crystal1 = crystalChoices[Math.floor(Math.random() * crystalChoices.length)];
+var crystal2 = crystalChoices[Math.floor(Math.random() * crystalChoices.length)];
+var crystal3 = crystalChoices[Math.floor(Math.random() * crystalChoices.length)];
+var crystal4 = crystalChoices[Math.floor(Math.random() * crystalChoices.length)];
+
+console.log(crystal1);
+console.log(crystal2);
+console.log(crystal3);
+console.log(crystal4);
+
+computerChoice.text('Your number to reach is ' + computerGuess);
+userScore.text('Your current score is: ' + score);
 
 
-// document.getElementById("demo").addEventListener("click", myFunction);
+$('.rocks1').on('click', function(){
+    computerChoice.text('Your number to reach is ' + computerGuess);
+    userResult.text('');
+    score = score + crystal1;
+    userScore.text('Your current score is: ' + score);
+    console.log("the value of #1 crstyal is: " + crystal1);
+    checkWin();
+}
+)
 
-// function myFunction() {
-//   document.getElementById("demo").innerHTML = crystalChoices;
+$('.rocks2').on('click', function(){
+    computerChoice.text('Your number to reach is ' + computerGuess);
+    userResult.text('');
+    score = score + crystal2;
+    userScore.text('Your current score is: ' + score);
+    console.log("the value of #1 crstyal is: " + crystal2);
+    checkWin();
+})
 
+$('.rocks3').on('click', function(){
+    computerChoice.text('Your number to reach is ' + computerGuess);
+    userResult.text('');
+    score = score + crystal3;
+    userScore.text('Your current score is: ' + score);
+    console.log("the value of #1 crstyal is: " + crystal3);
+    checkWin();
+})
 
+$('.rocks4').on('click', function(){
+    computerChoice.text('Your number to reach is ' + computerGuess);
+    userResult.text('');
+    score = score + crystal4;
+    userScore.text('Your current score is: ' + score);
+    console.log("the value of #1 crstyal is: " + crystal4);
+    checkWin();
+})
 
+function restart(){
+    score = 0;
+    computerGuess = Math.floor(Math.random() * 101) + 19;
+    crystal1 = crystalChoices[Math.floor(Math.random() * crystalChoices.length)];
+    crystal2 = crystalChoices[Math.floor(Math.random() * crystalChoices.length)];
+    crystal3 = crystalChoices[Math.floor(Math.random() * crystalChoices.length)];
+    crystal4 = crystalChoices[Math.floor(Math.random() * crystalChoices.length)];
+    computerChoice.text('Your number to reach is ' + computerGuess);}
 
-
-
-
-// for(var i = 0; )
+function checkWin(){
+if(score === computerGuess){
+    wins += 1;
+    userWins.text('Wins:' + wins);
+    userResult.text('You Win!');
+    // you calling restart function
+    restart();
+}
+else if(score > computerGuess){
+    losses += 1;
+    userLosses.text('Losses:' + losses);
+    userResult.text('You Lose');
+    // you calling restart function
+    restart();
+}
+}
